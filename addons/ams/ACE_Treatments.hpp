25b6a06d8c41e656a4f0921916e393ac6dd73af5
McDiod <hobbeldobbel@gmail.com>
2017-09-26 19:29:04 +0200

    add

---
+++ addons/ams/ACE_Treatments.hpp
@@ -0,0 +1,11 @@
+
+class ACE_Medical_Actions
+{
+     class Advanced
+     {
+          class CPR
+          {
+               treatmentTime = 8;
+          };
+     };
+};
---
+++ addons/ams/CfgFunctions.hpp
@@ -0,0 +1,12 @@
+class CfgFunctions
+{
+	class ACE_medical
+	{
+		tag = "ACE_medical";
+		class misc
+		{
+			file = "x\grad\addons\ams\functions";
+			class getBloodVolumeChange {};
+		};
+	};
+};
