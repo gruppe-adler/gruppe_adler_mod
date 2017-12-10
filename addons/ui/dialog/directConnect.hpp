ba30d38e23df8af2f3dd21e7a0fc1e12a0c791b6
McDiod <hobbeldobbel@gmail.com>
2017-12-10 22:01:02 +0100

    parametrize password window

--- addons/ui/dialog/directConnect.hpp
+++ addons/ui/dialog/directConnect.hpp
@@ -1,3 +1,5 @@
+#include "directConnectDefines.hpp"
+
 class RscButtonMenu;
 class RscVignette;
 class RscControlsGroupNoScrollbars;
@@ -5,6 +7,7 @@ class RscText;
 class RscButtonMenuCancel;
 class RscButtonMenuOK;
 class RscTitle;
+class RscEdit;
 
 class RscDisplayMain {
     class Controls {
@@ -45,7 +48,7 @@ class grad_RscDisplayDirectConnectPassword {
 	movingEnable=0;
 	enableDisplay=1;
 
-	class controlsBackground {
+	class ControlsBackground {
 		class Vignette: RscVignette {
 			idc=114998;
 		};
@@ -68,10 +71,6 @@ class grad_RscDisplayDirectConnectPassword {
 				};
 			};
 		};
-		class BackgroundDisable: RscText {
-		};
-		class BackgroundDisableTiles: RscText {
-		};
 		class RscTitleBackground: RscText {
 			colorBackground[]= {
 				"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.13])",
@@ -80,45 +79,64 @@ class grad_RscDisplayDirectConnectPassword {
 				"(profilenamespace getvariable ['GUI_BCG_RGB_A',0.8])"
 			};
 			idc=1080;
-			x="1 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
-			y="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
-			w="38 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+			x = GRAD_DIRECTCONNECT_TOTALX;
+			y = GRAD_DIRECTCONNECT_TOTALY;
+			w = GRAD_DIRECTCONNECT_TOTALW;
+			h = GRAD_DIRECTCONNECT_ELEMENTH;
 		};
 		class MainBackground: RscText
 		{
 			colorBackground[]={0,0,0,0.69999999};
 			idc=1082;
-			x="1 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
-			y="2.1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
-			w="38 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h="20.8 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+			x= GRAD_DIRECTCONNECT_TOTALX;
+			y = GRAD_DIRECTCONNECT_MAINY;
+			w = GRAD_DIRECTCONNECT_TOTALW;
+			h = GRAD_DIRECTCONNECT_MAINH;
 		};
 	};
-	class controls {
+
+	class Controls {
 		class Title: RscTitle {
 			colorBackground[]={0,0,0,0};
 			style=0;
 			idc=1000;
-			text = "ENTER PASSWORD";
-			x="1 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
-			y="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
-			w="15 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+			text = "CONNECT TO GRUPPE ADLER";
+            x = GRAD_DIRECTCONNECT_TOTALX;
+			y = GRAD_DIRECTCONNECT_TOTALY;
+            w = GRAD_DIRECTCONNECT_TOTALW;
+			h = GRAD_DIRECTCONNECT_TITLEH;
 		};
 
+        /*class Text: RscText {
+            idc=1008;
+            text="ENTER PASSWORD";
+            x="0 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            y="0 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+            w="8 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+            colorBackground[]={0,0,0,0};
+        };
+
+        class PasswordBox: RscEdit {
+            idc = 1009;
+            x="0 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            y="1.5 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+            w="8 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };*/
+
 		class ButtonCancel: RscButtonMenuCancel {
-			x="1 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
-			y="23 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
-			w="6.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+			x = GRAD_DIRECTCONNECT_TOTALX;
+			y = GRAD_DIRECTCONNECT_BUTTONY;
+			w = GRAD_DIRECTCONNECT_BUTTONW;
+			h = GRAD_DIRECTCONNECT_ELEMENTH;
 		};
 
 		class ButtonOK: RscButtonMenuOK {
-			x="32.75 * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX + (safezoneW - ((safezoneW / safezoneH) min 1.2))/2)";
-			y="23 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + (safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))/2)";
-			w="6.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h="1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+			x = GRAD_DIRECTCONNECT_BUTTONOKX;
+			y = GRAD_DIRECTCONNECT_BUTTONY;
+			w = GRAD_DIRECTCONNECT_BUTTONW;
+			h = GRAD_DIRECTCONNECT_ELEMENTH;
 		};
 	};
 };
