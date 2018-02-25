a85ec5fb8d25aae0fddc18560fb21baede999c2c
McDiod <hobbeldobbel@gmail.com>
2018-02-25 16:37:35 +0100

    add grad_axe

---
+++ addons/axe/CfgWeapons.hpp
@@ -0,0 +1,16 @@
+class CfgWeapons {
+    class CBA_MiscItem_ItemInfo;
+    class ACE_ItemCore;
+
+    class grad_axe: ACE_ItemCore {
+        author = "McDiod";
+        displayName = "$STR_GRAD_AXE_AXENAME";
+        descriptionShort = "$STR_GRAD_AXE_AXEDESCRIPTION";
+        /* model = QPATHTOF(data\ace_wirecutter.p3d); */
+        picture = "\x\grad\addons\axe\ui\item_axe_ca.paa";
+        scope = 2;
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 25;
+        };
+    };
+};
