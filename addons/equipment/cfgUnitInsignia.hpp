95372814c0a02b10d31f10134408c443637525f7
Synch <kevin.piontek@live.de>
2018-08-19 17:42:00 +0200

    Removed RHS dependancy; added Insignias
    Removed RHS eagle backpack and therefore RHS dependency
    Added flag insignias for countries where joint operation groups are located (+USA) for usage in mission scenarios and events
    
    Adjusted Kitbag - Medic version now uses Red Crystal as usual in vanilla Arma 3

---
+++ addons/equipment/cfgUnitInsignia.hpp
@@ -0,0 +1,53 @@
+class CfgUnitInsignia
+{
+	class ga_insignia_de
+	{
+		displayName = "Germany"; // Name displayed in Arsenal
+		author = "Synch"; // Author displayed in Arsenal
+		texture = "gx\grad\addons\equipment\data\insignia\de_v2.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+	
+	class ga_insignia_es
+	{
+		displayName = "Spain"; // Name displayed in Arsenal
+		author = "Synch"; // Author displayed in Arsenal
+		texture = "x\grad\addons\equipment\data\insignia\es_v2.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+	
+	class ga_insignia_it
+	{
+		displayName = "Italy"; // Name displayed in Arsenal
+		author = "Synch"; // Author displayed in Arsenal
+		texture = "x\grad\addons\equipment\data\it_v2.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+	
+	class ga_insignia_uk
+	{
+		displayName = "United Kingdom"; // Name displayed in Arsenal
+		author = "Synch"; // Author displayed in Arsenal
+		texture = "x\grad\addons\equipment\data\uk_v2.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+	
+	class ga_insignia_se
+	{
+		displayName = "Sweden"; // Name displayed in Arsenal
+		author = "Synch"; // Author displayed in Arsenal
+		texture = "x\grad\addons\equipment\data\se_v2.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+	
+	class ga_insignia_us
+	{
+		displayName = "United States of America"; // Name displayed in Arsenal
+		author = "Synch"; // Author displayed in Arsenal
+		texture = "x\grad\addons\equipment\data\us_v2.paa"; // Image path
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+	
+	
+	
+};
