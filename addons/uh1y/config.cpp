08557690270d49d198016682e1f01f8cd64752a7
nomisum <nomisum@googlemail.com>
2018-09-02 21:20:56 +0200

    inherit AH-1Z sounds to UH-1Y

--- addons/uh1y/config.cpp
+++ addons/uh1y/config.cpp
@@ -23,6 +23,54 @@ class CfgVehicles {
 
 	class RHS_UH1Y_base: RHS_UH1_Base {
 
+        class Sounds {
+
+            class EngineExt {
+                sound[] = {"rhsusf\addons\rhsusf_a2port_air2\sounds\ah1\ext\ext-ah1z-engine-01", 3.262278, 1, 800};
+                frequency = "rotorSpeed";
+                volume = "camPos*((rotorSpeed-0.72)*4)";
+            };
+
+            class EngineInt {
+                sound[] = {"rhsusf\addons\rhsusf_sounds\0_vehicles\ah-1\ah-1_int_engine", 1.400000, 1};
+                frequency = "rotorSpeed*(1+rotorThrust/6)*0.8";
+                volume = "(1-camPos)*((rotorSpeed-0.75)*4)";
+            };
+
+            class DistanceSound {
+                sound[] = {"rhsusf\addons\rhsusf_sounds\0_vehicles\ah-1\ah-1_distant", 2.310000, 1, 3200};
+                frequency = "rotorSpeed";
+                volume = "2 * camPos * (0 max (rotorSpeed-0.4))";
+            };
+        };
+
+        class SoundsExt {
+
+            class SoundEvents {
+            };
+
+            class Sounds: Sounds {
+
+                class EngineExt {
+                    sound[] = {"rhsusf\addons\rhsusf_a2port_air2\sounds\ah1\ext\ext-ah1z-engine-01", 2.762278, 1, 800};
+                    frequency = "rotorSpeed";
+                    volume = "camPos*((rotorSpeed-0.72)*4)";
+                };
+
+                class EngineInt {
+                    sound[] = {"rhsusf\addons\rhsusf_sounds\0_vehicles\ah-1\ah-1_int_engine", 1.400000, 1};
+                    frequency = "rotorSpeed*(1+rotorThrust/6)*0.8";
+                    volume = "(1-camPos)*((rotorSpeed-0.75)*4)";
+                };
+
+                class DistanceSound {
+                    sound[] = {"rhsusf\addons\rhsusf_sounds\0_vehicles\ah-1\ah-1_distant", 2.310000, 1, 3200};
+                    frequency = "rotorSpeed";
+                    volume = "2 * camPos * (0 max (rotorSpeed-0.4))";
+                };
+            };
+        };
+
         class Reflectors {
 
             class Middle {
