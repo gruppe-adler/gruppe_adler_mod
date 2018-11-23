875bca92234e68bbc991295ecd27e94d929785a1
McDiod <hobbeldobbel@gmail.com>
2018-11-23 13:35:07 +0100

    close #97 let CBA handle directConnect PW

--- addons/ui/cfgFunctions.hpp
+++ addons/ui/cfgFunctions.hpp
@@ -3,12 +3,10 @@ class CfgFunctions {
 		class directConnect {
 			file = QPATHTOF(functions\directConnect);
 			class connectToServer {};
-      class onButtonConnect_rightClick {};
+            class onButtonConnect_rightClick {};
 			class onButtonConnect {};
-			class onButtonPasswordOK {};
 			class onButtonPortOK {};
 			class onLoadButtonConnect {};
-			class onLoadRscDisplayDirectConnectPassword {};
 		};
 	};
 };
