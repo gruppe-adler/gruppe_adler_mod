4ac8a700641238a5062c7bf112e6b3a29910b46f
Salbei <salbei@gruppe-adler.de>
2019-01-17 20:39:59 +0100

    fixes

--- addons/getLoadoutsFromUnits/Display3DEN.hpp
+++ addons/getLoadoutsFromUnits/Display3DEN.hpp
@@ -1,18 +1,16 @@
 class Display3DEN {
-  class ctrlDefault;
-  class ctrlDefaultText: ctrlDefault {};
-  class ctrlMenu: ctrlDefaultText {};
-  class ContextMenu: ctrlMenu {
-    class Items {
-      class Log {
-        items[] = {"LogPosition", "LogClasses", "GRAD_getLoadoutsFromUnits"};
-      };
-      class GRAD_getLoadoutsFromUnits {
-        text = "Copy Grad-Loadout to Clipboard";
-        action = QUOTE([] call FUNC(createConfig));
-        conditionShow = "selectedObject";
-        value = 0;
-      };
+    class ctrlMenu;
+    class ContextMenu: ctrlMenu {
+        class Items {
+            class Log {
+                items[] = {"LogPosition", "LogClasses", "GRAD_getLoadoutsFromUnits"};
+            };
+            class GRAD_getLoadoutsFromUnits {
+                text = "Copy Grad-Loadout to Clipboard";
+                action = QUOTE([] call FUNC(createConfig));
+                conditionShow = "selectedObject";
+                value = 0;
+            };
+        };
     };
-  };
 };
