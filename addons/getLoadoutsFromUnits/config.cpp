01ce489046f020d745e078b2f93cccc599a690ec
Salbei <salbei@gruppe-adler.de>
2019-01-20 01:03:06 +0100

    removed ace_main

--- addons/getLoadoutsFromUnits/config.cpp
+++ addons/getLoadoutsFromUnits/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"grad_main", "ace_common"};
+        requiredAddons[] = {"grad_main"};
         authors[] = { "Salbei"};
         VERSION_CONFIG;
     };
