b937d8d875a3e7e113c3e6dc58d4abdd6979cd8a
Sven <sven0215@hotmail.com>
2019-02-21 20:10:19 +0100

    fixed  intentatio

--- addons/grenadeLauncherAmmoRhs/config.cpp
+++ addons/grenadeLauncherAmmoRhs/config.cpp
@@ -1,19 +1,19 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-	class ADDON {
-		author = "$STR_grad_Author";
-		name = QUOTE(ADDON);
-		url = "$STR_grad_URL";
-		requiredVersion = 1.0;
-		requiredAddons[] = {"grad_main", "A3_Weapons_F", "rhsusf_c_weapons"};
-		units[] = {};
-		weapons[] = {};
-		VERSION_CONFIG;
+    class ADDON {
+        author = "$STR_grad_Author";
+        name = QUOTE(ADDON);
+        url = "$STR_grad_URL";
+        requiredVersion = 1.0;
+        requiredAddons[] = {"grad_main", "A3_Weapons_F", "rhsusf_c_weapons"};
+        units[] = {};
+        weapons[] = {};
+        VERSION_CONFIG;
         authors[] = {
-			"Fusselwurm <fusselwurm@gmail.com>"
-		};
-	};
+            "Fusselwurm <fusselwurm@gmail.com>"
+        };
+    };
 };
 
 #include "cfgAmmo.hpp"
