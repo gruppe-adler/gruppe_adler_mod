bd5f41bbb723703d50f1aaae2933e6a20f220274
Willard <sven0215@hotmail.com>
2019-09-25 15:10:39 +0200

    added support for m32 smoke grenades

--- addons/grenadeLauncherAmmoRhs/cfgAmmo.hpp
+++ addons/grenadeLauncherAmmoRhs/cfgAmmo.hpp
@@ -5,9 +5,19 @@ class CfgAmmo {
         simulation = "shotSmoke";
         deflectionSlowDown = 0.4;
     };
+    class rhs_40mm_m714_white: SmokeShell
+    {
+        simulation = "shotSmoke";
+        deflectionSlowDown = 0.4;
+    };
 
     class SmokeShellRed;
     class rhs_40mm_smoke_red: SmokeShellRed
+    {
+        simulation = "shotSmoke";
+        deflectionSlowDown = 0.4;
+    };
+    class rhs_40mm_m713_red: SmokeShellRed
     {
         simulation = "shotSmoke";
         deflectionSlowDown = 0.4;
@@ -19,9 +29,19 @@ class CfgAmmo {
         simulation = "shotSmoke";
         deflectionSlowDown = 0.4;
     };
+    class rhs_40mm_m715_green: SmokeShellGreen
+    {
+        simulation = "shotSmoke";
+        deflectionSlowDown = 0.4;
+    };
 
     class SmokeShellYellow;
     class rhs_40mm_smoke_yellow: SmokeShellYellow
+    {
+        simulation = "shotSmoke";
+        deflectionSlowDown = 0.4;
+    };
+    class rhs_40mm_m716_yellow: SmokeShellYellow
     {
         simulation = "shotSmoke";
         deflectionSlowDown = 0.4;
@@ -33,5 +53,5 @@ class CfgAmmo {
         simulation = "shotSmoke";
         deflecting = 30;
         deflectionSlowDown = 0.4;
-    }
+    };
 };
