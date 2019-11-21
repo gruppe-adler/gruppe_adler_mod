18b72c71736f61f8a9604da09783a9d33c5ab4fb
Moritz Schmidt <fusselwurm@gmail.com>
2019-11-21 22:14:28 +0100

    tweak action points

--- addons/animalTransport/CfgVehicles.hpp
+++ addons/animalTransport/CfgVehicles.hpp
@@ -14,7 +14,7 @@ class CfgVehicles {
     class Van_01_base_F;
     class I_G_Van_01_transport_F: Van_01_base_F {
 		class GRAD_AnimalTransport {
-            unloadPoint[] = {0, -4.0, -0.6};
+            unloadPoint[] = {0, -4.3, -0.6};
             unloadActionPoint[] = {0, -3.5, -0.4};
             class Sheep_random_F {
                 class Spaces {
@@ -58,8 +58,8 @@ class CfgVehicles {
     class RHS_Ural_Civ_Base;
     class rhsgref_ins_ural_work_open: RHS_Ural_Civ_Base {
 		class GRAD_AnimalTransport {
-            unloadPoint[] = {0, -4.0, -0.2};
-            unloadActionPoint[] = {0, -3.2, -0.1};
+            unloadPoint[] = {0, -4.1, -0.2};
+            unloadActionPoint[] = {0, -3.3, -0.1};
             class Sheep_random_F {
                 class Spaces {
                     class FrontLeft {
@@ -103,8 +103,8 @@ class CfgVehicles {
     class rhs_gaz66_vmf;
     class rhs_gaz66o_vmf: rhs_gaz66_vmf {
         class GRAD_AnimalTransport {
-            unloadPoint[] = {0, -3.0, -0.6};
-            unloadActionPoint[] = {0, -2.5, -0.4};
+            unloadPoint[] = {0, -3.4, -0.6};
+            unloadActionPoint[] = {0, -2.6, -0.4};
             class Sheep_random_F {
                 class Spaces {
                     class FrontOuterLeft {
@@ -138,7 +138,7 @@ class CfgVehicles {
     class rhs_zil131_open_base;
     class rhs_zil131_flatbed_base: rhs_zil131_open_base {
         class GRAD_AnimalTransport {
-            unloadPoint[] = {0, -3.2, -0.6};
+            unloadPoint[] = {0, -3.4, -0.6};
             unloadActionPoint[] = {0, -2.6, -0.4};
             class Sheep_random_F {
                 class Spaces {
