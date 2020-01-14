d956f071fde65577f06fc58ba3e4096afcc1d0d1
Synch2k <kevin.piontek@live.de>
2020-01-15 00:13:01 +0100

    DRAFT

--- addons/kit/cfgVehicles.hpp
+++ addons/kit/cfgVehicles.hpp
@@ -3,6 +3,8 @@ class CfgVehicles
 	class B_Soldier_F;
 	class B_Soldier_lite_F;
 	class B_soldier_AR_F;
+	class B_GEN_Soldier_F;
+	class I_E_Soldier_lite_F;
 
 	class B_Kitbag_rgr;
 	class B_Messenger_Coyote_F;
@@ -10,7 +12,13 @@ class CfgVehicles
 	class B_Messenger_Olive_F;
 	class B_Messenger_IDAP_F;
 	
-	
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+
+
 	class grad_soldier_combatUniform_usArmy_OCP: B_Soldier_F
 	{
 		author = "Synch";
@@ -24,7 +32,6 @@ class CfgVehicles
         {
             "x\grad\addons\kit\data\uniforms\combatUniform\USArmy\GRAD_U_CombatUniform_USArmy_OCP.paa"
         };
-		
 	};
 
 	class grad_soldier_combatUniform_usArmy_OCP_S: B_Soldier_lite_F
@@ -38,7 +45,7 @@ class CfgVehicles
         };
 		hiddenSelectionsTextures[]=
         {
-            "x\grad\addons\kit\data\uniforms\combatUniform\USArmy\GRAD_U_CombatUniform_USArmy_OCP.paa"
+            "x\grad\addons\kit\data\uniforms\combatUniform\USArmy\GRAD_U_CombatUniform_USArmy_OCP_v2.paa" //TEMP V2
         };
 		
 	};
@@ -59,7 +66,103 @@ class CfgVehicles
 		
 	};
 
+
+
+class grad_soldier_combatUniform_noFlag_Khaki: B_Soldier_F
+	{
+		author = "Synch";
+		scope = 1;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).		
+		uniformClass = "grad_u_combatUniform_noFlag_Khaki";
+		hiddenSelectionsMaterials[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatUniform\GRAD_U_CombatUniform_noFlag.rvmat"
+        };
+		hiddenSelectionsTextures[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa"
+        };
+	};
+
+	class grad_soldier_combatUniform_noFlag_Khaki_S: B_Soldier_lite_F
+	{
+		author = "Synch";
+		scope = 1;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).		
+		uniformClass = "grad_u_combatUniform_noFlag_Khaki_S";
+		hiddenSelectionsMaterials[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatUniform\GRAD_U_CombatUniform_noFlag.rvmat"
+        };
+		hiddenSelectionsTextures[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa" //TEMP V2
+        };
+		
+	};
+
+	class grad_soldier_combatUniform_noFlag_Khaki_T: B_soldier_AR_F
+	{
+		author = "Synch";
+		scope = 1;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).		
+		uniformClass = "grad_u_combatUniform_noFlag_Khaki_T";
+		hiddenSelectionsMaterials[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatUniform\GRAD_U_CombatUniform_noFlag.rvmat"
+        };
+		hiddenSelectionsTextures[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa"
+        };
+		
+	};
+
+
+
+
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+
+
+
+
+	class grad_soldier_combatCasual_khaki_blue: B_GEN_Soldier_F
+	{
+		author = "Synch";
+		scope = 1;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).		
+		uniformClass = "grad_u_combatCasual_khaki_blue";
+		hiddenSelectionsTextures[]=
+        {
+            "x\grad\addons\kit\data\uniforms\combatCasual\grad_u_combatCasual_shirt_blue.paa","x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa" //2_1 & 2_2 & 2_3
+        };
+		
+	};
+
 	
+
+
+
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+
+class grad_soldier_combatFatigues_khaki: I_E_Soldier_lite_F
+	{
+		author = "Synch";
+		scope = 1;                          // 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).		
+		uniformClass = "grad_u_combatFatigues_khaki";
+		// hiddenSelectionsTextures[]=
+        // {
+        //     "x\grad\addons\kit\data\uniforms\combatCasual\grad_u_combatCasual_shirt_blue.paa","x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa","x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa" //2_1 & 2_2 & 2_3
+        // };
+		
+	};
+
+
+
 
 	class grad_kitbag_black: B_Kitbag_rgr
 	{
--- addons/kit/cfgWeapons.hpp
+++ addons/kit/cfgWeapons.hpp
@@ -4,10 +4,24 @@ class CfgWeapons
 	//class U_I_G_Story_Protagonist_F;
 	
 	
-	class U_B_CombatUniform_mcam: Uniform_Base
+	class U_B_CombatUniform_mcam: Uniform_Base //Combat Uniform
 	{
 		class ItemInfo;
 	};
+	class U_B_GEN_Soldier_F: Uniform_Base //Combat Casual
+	{
+		class ItemInfo;
+	}
+	class U_I_E_Uniform_01_tanktop_F: Uniform_Base //Combat Fatigues (Tank Top, Gloves)
+	{
+		class ItemInfo;
+	}
+
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
+	// COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START COMBAT UNIFORM START
 
 	class grad_u_combatUniform_usArmy_OCP: U_B_CombatUniform_mcam
 	{
@@ -38,5 +52,101 @@ class CfgWeapons
             uniformClass = "grad_soldier_combatUniform_usArmy_OCP_T";
         };
 	};
+
+class grad_u_combatUniform_noFlag_Khaki: U_B_CombatUniform_mcam
+	{
+		author = "Synch";
+		displayName = "Combat Uniform (Khaki)";		
+		class ItemInfo: ItemInfo
+        {
+            uniformClass = "grad_soldier_combatUniform_noFlag_Khaki";
+        };
+	};
+
+	class grad_u_combatUniform_noFlag_Khaki_S: U_B_CombatUniform_mcam
+	{
+		author = "Synch";
+		displayName = "Combat Uniform (Khaki, Rolled-up)";		
+		class ItemInfo: ItemInfo
+        {
+            uniformClass = "grad_soldier_combatUniform_noFlag_Khaki_S";
+        };
+	};
+
+	class grad_u_combatUniform_noFlag_Khaki_T: U_B_CombatUniform_mcam
+	{
+		author = "Synch";
+		displayName = "Combat Uniform (Khaki, Tee)";		
+		class ItemInfo: ItemInfo
+        {
+            uniformClass = "grad_soldier_combatUniform_noFlag_Khaki_T";
+        };
+	};
+
+
+
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+// COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START COMBAT CASUAL START
+
+
+
+
+	class grad_u_combatCasual_khaki_blue: U_B_GEN_Soldier_F
+	{
+		author = "Synch";
+		displayName = "Combat Casual (Khaki, Blue)";		
+		class ItemInfo: ItemInfo
+        {
+            uniformClass = "grad_soldier_combatCasual_khaki_blue";
+        };
+	};
+
+
+
+
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+// COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START COMBAT FATIGUES START
+
+
+class grad_u_combatFatigues_khaki: U_I_E_Uniform_01_tanktop_F
+	{
+		author = "Synch";
+		displayName = "Combat Fatigues (Khaki)";		
+		hiddenSelections[] = {"camo1","camo2","camo3","insignia"};
+		//hiddenSelectionsTextures[] = {"x\grad\addons\kit\data\uniforms\combatCasual\grad_u_combatCasual_shirt_blue.paa","x\grad\addons\kit\data\uniforms\combatUniform\noFlag\GRAD_U_CombatUniform_noFlag_Khaki.paa","\A3\Characters_F_Enoch\Uniforms\Data\I_E_Soldier_01_gloves_black_CO.paa"};
+		hiddenSelectionsTextures[] = {"LDF_Desert_Uniforms\Data\ACM_T_U_Tanktop.paa","LDF_Desert_Uniforms\Data\ACM_T_U_Soldier1.paa","\A3\Characters_F_Enoch\Uniforms\Data\I_E_Soldier_01_gloves_black_CO.paa"};
+		class ItemInfo: ItemInfo
+        {
+            uniformClass = "grad_soldier_combatFatigues_khaki";
+        };
+	};
+
+
+	// //CARRIER
+	// class ItemCore;
+	// class InventoryItem_Base_F;
+	// class VestItem;
+	// class Vest_Camo_Base;
+	// class V_PlateCarrier1_blk: Vest_Camo_Base
+	// {
+	// 	class ItemInfo;
+	// };
+
+	// class grad_v_carrierLight_OCP: V_PlateCarrier1_blk
+	// {
+	// 	author = "Synch";
+	// 	displayName = "Carrier Light (OCP)";
+	// 	hiddenSelectionsTextures[] = {"x\grad\addons\kit\data\vests\grad_V_CarrierLight_OCP.paa"};
+	// };
+
+
+
+
 
 };
