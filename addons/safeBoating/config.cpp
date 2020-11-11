b83d5b030a17af1e3af403fcb714bcd0fb3bfd92
Moritz Schmidt <fusselwurm@gmail.com>
2020-11-11 21:55:41 +0100

    hmmm 0.3 is still ok

--- addons/safeBoating/config.cpp
+++ addons/safeBoating/config.cpp
@@ -20,6 +20,6 @@ class CfgPatches {
 class CfgVehicles {
 	class Boat_Transport_02_base_F;
 	class Rubber_duck_base_F: Boat_Transport_02_base_F {
-		rudderForceCoef = 0.5;
+		rudderForceCoef = 0.3;
 	};
 };
