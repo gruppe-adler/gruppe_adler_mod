d53757caf291790bf197f9c1c243437dc2005af5
Moritz Schmidt <fusselwurm@gmail.com>
2021-06-13 19:46:35 +0200

    use ACE_player instead of player

--- addons/clutterCutter/CfgVehicles.hpp
+++ addons/clutterCutter/CfgVehicles.hpp
@@ -5,9 +5,9 @@ class CfgVehicles {
 			class ACE_Equipment {
 				class AFI_CutterAction {
 					displayName = "Remove clutter";
-					condition = "player == (vehicle player)";
+					condition = "ACE_player == (vehicle ACE_player)";
 					exceptions[] = {};
-					statement = "player playActionNow 'medic'; [4, [], {_obj = createVehicle ['Land_ClutterCutter_medium_F', position player, [], 0, 'CAN_COLLIDE'];}, {player playActionNow 'medicstop';}, 'Removing clutter...'] call ace_common_fnc_progressBar";
+					statement = "ACE_player playActionNow 'medic'; [4, [], {_obj = createVehicle ['Land_ClutterCutter_medium_F', position ACE_player, [], 0, 'CAN_COLLIDE'];}, {ACE_player playActionNow 'medicstop';}, 'Removing clutter...'] call ace_common_fnc_progressBar";
 					icon = "\x\grad\addons\clutterCutter\ui\cutterIcon.paa";
 				};
 			};
