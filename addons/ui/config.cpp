082049d5f106ae8bbdf31950919b17bb5b8a92fe
Willard <sven0215@hotmail.com>
2025-07-07 19:39:52 +0200

    fix: tabs

--- addons/ui/config.cpp
+++ addons/ui/config.cpp
@@ -1,20 +1,20 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-	class ADDON {
-		author = "$STR_grad_Author";
-		name = QUOTE(ADDON);
-		url = "$STR_grad_URL";
-		requiredVersion = 1.0;
-		requiredAddons[] = {"grad_main","A3_ui_f", "cba_ui"};
-		units[] = {};
-		weapons[] = {};
-		VERSION_CONFIG;
+    class ADDON {
+        author = "$STR_grad_Author";
+        name = QUOTE(ADDON);
+        url = "$STR_grad_URL";
+        requiredVersion = 1.0;
+        requiredAddons[] = {"grad_main","A3_ui_f", "cba_ui"};
+        units[] = {};
+        weapons[] = {};
+        VERSION_CONFIG;
         authors[] = {
-			"McDiod",
-      		"DerZade"
-		};
-	};
+            "McDiod",
+            "DerZade"
+        };
+    };
 };
 
 #include "cfgFunctions.hpp"
